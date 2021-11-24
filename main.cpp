38e4c97b6009d2622fb6303f3f79a58cdb7eb2fd
AAA <eat.myass@exmple.com>
2021-11-24 23:22:02 +0100

    1

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <filesystem>
 
@@ -6,6 +7,8 @@ namespace fs = std::filesystem;
 
 int main(int argc, char const *argv[])
 {
-    
+    std::string path = "/path/to/directory";
+    for (const auto & entry : fs::directory_iterator(path))
+    std::cout << entry.path() << std::endl;
     return 0;
 }
